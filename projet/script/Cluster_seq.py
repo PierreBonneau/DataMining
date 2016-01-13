341dbf98084b078d7624c9b693eda6bd46fc82b5
Myriam Lopez <mlopez001006@emi.u-bordeaux1.fr>
2016-01-13 21:19:06 +0100

    ok bon transfert du script version qui fonctionne pour generer les fasta ...

--- projet/script/Cluster_seq.py
+++ projet/script/Cluster_seq.py
@@ -4,18 +4,16 @@ from Bio import Phylo
 from Bio import SeqIO
 from Bio.Align.Applications import ClustalwCommandline
 
-f2 = open('uniprot_riz.xml', 'r')
-f3 = open('uniprot_tomate.xml', 'r')
-
 nb = 1
 
 groupes = {}
 organism = ""
- 
 
+##for record in SeqIO.parse(f2, 'uniprot-xml') :
+##    if record.name == 'K4CDJ0_SOLLC' :
+##        print record.seq
 
 def extraction_grp(j):
-    print "coucou ! "
     org = 0
     grp = {}
     f1 = open('./../tables/cluster'+str(j)+'.csv', 'r')
@@ -31,18 +29,25 @@ def extraction_grp(j):
     return grp
         
 for j in range(15) :
-    groupes = extraction_grp(j)
-    fgroupe = open("sequences_groupe"+str(nb)+".fasta", 'w')
-    for i in groupes.keys():
-        if groupes[i] == 1:
-            for record in SeqIO.parse(f3, 'uniprot-xml') :
-                if record.name == i :
-                    fgroupe.write(">Solanum|"+str(record.name)+"\n"+str(record.seq)+"\n\n")
-        if groupes[i] == 0:
-            for record in SeqIO.parse(f2, 'uniprot-xml') :
-                if record.name == i :
-                    fgroupe.write(">Oriza|"+str(record.name)+"\n"+str(record.seq)+"\n\n")
-    nb += 1
+	groupes = extraction_grp(j)
+    	fgroupe = open("./fasta/sequences_groupe"+str(nb)+".fasta", 'w')
+    	for i in groupes.keys():
+        	if groupes[i] == 1:
+			f3 = open('uniprot_tomate.xml', 'r')
+			for record in SeqIO.parse(f3, 'uniprot-xml') :
+                		if record.name == i :
+                    			fgroupe.write(">Solanum|"+str(record.name)+"\n"+str(record.seq)+"\n\n")
+        		f3.close()
+		elif groupes[i] == 0:
+			f2 = open('uniprot_riz.xml', 'r')
+			for record in SeqIO.parse(f2, 'uniprot-xml') :
+				if record.name == i :
+					fgroupe.write(">Oriza|"+str(record.name)+"\n"+str(record.seq)+"\n\n")
+    			f2.close()
+	nb += 1
+    	fgroupe.close()
+
+
                 
 ##clustalw_exe = r"/net/cremi/mlopez001006/Bureau/clustalw2"
 ##
